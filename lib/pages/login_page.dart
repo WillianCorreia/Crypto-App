25847e6591e74ea5ea77337dda4f82e2f612eac2
Willian Prestes Correia Cellos <cellos.willian@gmail.com>
2022-06-08 01:14:34 -0300

    firebase tela login_part2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,11 +10,132 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final email = TextEditingController();
+  final senha = TextEditingController();
+
+  bool isLogin = true;
+  late String titulo;
+  late String actionButton;
+  late String toogleButton;
+
+
+  //Inicializar o estado
+  @override
+  void initState() {
+    super.initState();
+    setFormAction(true);
+  }
+
+  //Setar o formulario
+  setFormAction(bool acao) {
+    setState(() {
+      isLogin = acao;
+      //Login
+      if(isLogin) {
+        titulo = 'Bem Vindo';
+        actionButton = 'Login';
+        toogleButton = 'Ainda não tem conta ? Cadastre-se agora.';
+      //Cadastro
+      } else {
+        titulo = 'Crie sua conta';
+        actionButton = 'Cadastrar';
+        toogleButton = 'Voltar ao Login.';
+      }
+    });
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 100),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //TITULO
+                Text(
+                  titulo,
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: -1.5,
+                  ),
+                ),
+                //EMAIL
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: TextFormField(
+                    controller: email,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'E-mail',
+                    ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                      if(value!.isEmpty) {
+                        return 'Informar o e-mail';
+                      } else {
+                        return null;
+                      }
+                  },
+                  ),
+                ),
+                //SENHA
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: TextFormField(
+                    controller: senha,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Senha',
+                    ),
+                    validator: (value) {
+                      if(value!.isEmpty) {
+                        return 'Informar a senha';
+                      } else if(value.length < 6) {
+                        return 'Sua senha deve possuir no mínimo 6 caracteres';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                //BOTAO
+                Padding(
+                  padding: EdgeInsets.all(24),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.check),
+                        Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text(
+                            actionButton,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //TOGGLE
+                TextButton(
+                    onPressed: () => setFormAction(!isLogin),
+                    child: Text(toogleButton)
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
